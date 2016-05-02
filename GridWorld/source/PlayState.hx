dab16e77c1d970d2d2cf5e8ae413919aa5b87a5a
Aaron Nech <aaronnech@gmail.com>
2016-05-02 14:50:46 -0700

    fixed

--- GridWorld/source/PlayState.hx
+++ GridWorld/source/PlayState.hx
@@ -9,10 +9,12 @@ import flixel.math.FlxMath;
 import flixel.math.FlxVelocity;
 import flixel.system.FlxSound;
 import flixel.util.FlxColor;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState {
 	private var level:Level;
 	private var mainSimulator:Simulator;
+	private var tileMap:FlxTilemap;
 
 	public function new(level:Level) {
 		super();
@@ -24,6 +26,17 @@ class PlayState extends FlxState {
 		FlxG.mouse.visible = false;
 
 		this.mainSimulator = new Simulator(28, 15, this.level);
+
+		// // Background tile map
+		// var str:String = '';
+		// for (arr in this.level.getGrid()) {
+		// 	for (num in arr) {
+		// 		str = str + num + ',';
+		// 	}
+		// }
+		// str = str.substr(0, str):String
+		// this.tileMap = new FlxTilemap();
+		// this.tileMap.loadMap(MapData:String, TileGraphic:Class, TileWidth:uint = 0, TileHeight:uint = 0, AutoTile:uint, StartingIndex:uint = 0, DrawIndex:uint = 1, CollideIndex:uint = 1)
 	}
 
 	override public function update(elapsed:Float):Void {
