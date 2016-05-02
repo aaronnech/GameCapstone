1cad418a6d1cb3b78d06ae073c45af736a045787
Aaron Nech <aaronnech@gmail.com>
2016-05-02 13:26:37 -0700

    fixed

--- GridWorld/source/MenuState.hx
+++ GridWorld/source/MenuState.hx
@@ -9,9 +9,16 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
+	private var level:Level;
+
 	override public function create():Void
 	{
 		super.create();
+		this.level = new Level('assets/levels/level.1.json');
+		this.level.load(function() {
+			trace(this.level.getGrid());
+			trace(this.level.getObjects());
+		});
 	}
 
 	override public function update(elapsed:Float):Void
