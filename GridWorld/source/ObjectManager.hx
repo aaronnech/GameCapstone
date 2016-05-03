61ae1385ce71f9afda9a8ff4342b54e0f655f872
Diana Yeung <dianay@uw.edu>
2016-05-02 19:13:23 -0700

    hide goal

--- GridWorld/source/ObjectManager.hx
+++ GridWorld/source/ObjectManager.hx
@@ -55,6 +55,7 @@ class ObjectManager implements SpriteManager {
         updateSprite(this.vehicles, this.simulator.getVehicles());
         updateSprite(this.gems, this.simulator.getGems());
         updateSprite(this.goals, this.simulator.getGoals());
+        haxe.Timer.delay(checkGoal, Std.int(PlayState.TICK_TIME * 1000));
     }
 
     private function updateSprite(map:HashMap<Dynamic, FlxSprite>, obj:Array<Dynamic>) {
@@ -64,6 +65,17 @@ class ObjectManager implements SpriteManager {
             var newY = o.y * tileSize + Y_OFFSET;
             FlxTween.tween(map.get(o), {x: newX, y:newY}, PlayState.TICK_TIME / 2);
             // FlxTween.angle(map.get(o))
+        }
+    }
+
+    private function checkGoal() {
+        var gemObjs = this.simulator.getGems();
+        for (i in 0...gemObjs.length) {
+            var gem = gemObjs[i];
+            if (gem.isInGoal) {
+                this.gems.get(gem).visible = false;
+                this.goals.get(gem.parentGoal).visible = false;
+            }
         }
     }
 
