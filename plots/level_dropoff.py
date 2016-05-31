77f3b24ad45fc55aef0800b03b64425a56237309
Diana Yeung <dianay@uw.edu>
2016-05-30 20:08:05 -0700

    plot level dropoff

--- plots/level_dropoff.py
+++ plots/level_dropoff.py
@@ -13,9 +13,8 @@ def normalize(v):
 conn = sqlite3.connect(sys.argv[1])
 c = conn.cursor()
 
-c.execute('SELECT * FROM all_data WHERE event_name="level-start"')
+c.execute('SELECT * FROM all_data WHERE event_name="level-start" GROUP BY user_id, screen_name')
 a = c.fetchall()
-
 
 aNotEnabled = [t for t in a if t[10] == 0]
 aEnabled = [t for t in a if t[10] == 1]
@@ -27,13 +26,14 @@ for t in aEnabled:
 aHistogram = normalize(aHistogram)
 
 notAHistogram = np.zeros(24)
-for t in aEnabled:
+for t in aNotEnabled:
 	num = int(re.findall(r'\d+', t[4])[0])
 	notAHistogram[num] += 1
 notAHistogram = normalize(notAHistogram)
 
-
-#plt.plot(aHistogram)
-plt.plot(notAHistogram)
-# plt.plot(notAHistogram)
+plt.xlabel("levels")
+plt.xticks(range(24))
+plt.plot(aHistogram, '-r', label="Improved tutorial")
+plt.plot(notAHistogram, '-b', label="Old tutorial")
+plt.legend()
 plt.show()
